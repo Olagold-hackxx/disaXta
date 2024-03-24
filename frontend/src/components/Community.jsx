65bbe0bc2e973f4f7fe695364772eddc95246a07
TaiwoForbes <Soyemihissa2000@gmail.com>
2024-03-24 12:49:06 +0100

    changes

--- frontend/src/components/Community.jsx
+++ frontend/src/components/Community.jsx
@@ -4,8 +4,8 @@ import Postcomponent from './Postcomponent'
 const Community = () => {
   return (
     <div className='text-2xl text-center  pt-1 md:pt-5 '>
-        <h2 className=' text-left ml-5 font-semibold '>Community</h2>
-      <div className='pb-1 md:pb-3 text-lg md:text-xl font-semibold'/>
+        <h2 className=' text-left  border-b-2  p-6  font-semibold '>Community</h2>
+      <div className='pb-1 md:pb-3 text-lg md:text-xl border-b-2 font-semibold'/>
         <Postcomponent category='community' />
     </div>
   )
--- frontend/src/components/ShareLayout.jsx
+++ frontend/src/components/ShareLayout.jsx
@@ -7,8 +7,7 @@ import Cookies from "js-cookie";
 
 
 const SharedLayout = () => {
-  const user = JSON.parse(Cookies.get("user"));
-  console.log(user)
+  
   
   return (
     <div className='bg-gradient-to-r from-slate-900 to-slate-700 text-white h-screen'>
@@ -16,7 +15,7 @@ const SharedLayout = () => {
         <Topbar />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] px-2 md:px-10 ">
-        <Leftsidebar user={user} />
+        <Leftsidebar />
         {/* <Mainfeed/> */}
         <Outlet />
         <Rightsidebar />
